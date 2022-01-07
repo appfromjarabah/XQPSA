95a9e4eb9b7191088091ce7158913f3633b825c8
Mohammad Jarab'ah <96987889+appfromjarabah@users.noreply.github.com>
2022-01-07 18:59:40 +0200

    Update OR-Tools-solution-for-n-queen-problem.py

--- Codes/OR-Tools-solution-for-n-queen-problem.py
+++ Codes/OR-Tools-solution-for-n-queen-problem.py
@@ -1,5 +1,4 @@
 ''' OR-Tools Solution to The N-Queens Problem. '''
-import sys
 from ortools.constraint_solver import pywrapcp
 
 
@@ -19,16 +18,13 @@ def main(board_size):
     solver.Add(solver.AllDifferent([queens[i] + i for i in range(board_size)]))
     solver.Add(solver.AllDifferent([queens[i] - i for i in range(board_size)]))
 
+    # ADD THE DECISION BUILDER.
     db = solver.Phase(queens, solver.CHOOSE_FIRST_UNBOUND, solver.ASSIGN_MIN_VALUE)
 
-    # ITERATES THROUGH THE SOLUTIONS, DISPLAYING EACH.
+    # ITERATES THROUGH THE SOLUTIONS, SAVING EACH.
+    allSolutions = []
     num_solutions = 0
     solver.NewSearch(db)
-
-    # ALL SOLUTIONS LIST.
-    allSolutions = []
-    
-    # SAVE THE SOLUTIONS.
     while solver.NextSolution():
         singleSolution = []
         for i in range(board_size):
@@ -39,7 +35,7 @@ def main(board_size):
         num_solutions += 1
     solver.EndSearch()
 
-    # REVERSE THE LIST
+    # REVERSE THE LIST.
     print(f'{board_size}-Queen:')
     print('Start reversing the list ..')
     reversedList = []
@@ -58,14 +54,12 @@ def main(board_size):
 
 
 def SaveAllSols(NUM_OF_QUEENS):
-    # CREATE THE LISTS ==> """ _4_queen_solutions_list, .., _N_queen_solutions_list """.
+    # CREATE THE LISTS ==> ||| _4_queen_solutions_list, ..., _N_queen_solutions_list |||.
     for I in range(4, NUM_OF_QUEENS+1):
         board_size = I
-        if len(sys.argv) > 1:
-            board_size = int(sys.argv[1])
         main(board_size)
 
-    # CREATE THE LIST ==> """ ALL_SOLS_LIST = [_4_queen_solutions_list, .., _N_queen_solutions_list] """.
+    # CREATE THE LIST ==> ||| ALL_SOLS_LIST = [_4_queen_solutions_list, ..., _N_queen_solutions_list] |||.
     with open('db.py', mode='a', encoding='utf-8') as file:
         file.write('ALL_SOLS_LIST = [')
         for J in range(4, NUM_OF_QUEENS+1):
@@ -75,7 +69,7 @@ def SaveAllSols(NUM_OF_QUEENS):
                 file.write(f'_{J}_queen_solutions_list')
         file.write(f']')
     
-    # PRINT """ Finished! """, THAT REFERS TO FINISHED THE CODE!
+    # PRINT "Finished!", THAT REFERS TO FINISHED THE CODE!
     print('Finished!')
 
 
